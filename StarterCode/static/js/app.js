61ecb2321fd1d60909a54abc4527f0aa939683f9
Gina Ramirez <67246653+ramirezgc@users.noreply.github.com>
2020-10-19 22:14:05 -0500

    Create select options

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,29 +1,19 @@
+let select_tag = d3.select("#selDataset");
 
 d3.json("../data/samples.json").then((importedData) => {
-    let data = importedData});
+  let data = importedData.names;
+  console.log(data)
 
-    let trace1 = {
-        x: data.map(row => sample.otu_ids),
-        y: data.map(row => sample.sample_values),
-        text: data.map(row => sample.greekName),
-        name: "Test",
-        type: "bar",
-        orientation: "h"
-      };
-    
-      // data
-      let chartData = [trace1];
-    
-      // Apply the group bar mode to the layout
-      let layout = {
-        title: "Test",
-        margin: {
-          l: 100,
-          r: 100,
-          t: 100,
-          b: 100
-        }
-      };
-    
-      
-      Plotly.newPlot("plot", chartData, layout);
+  data.forEach((id) => {
+    select_tag
+      .append("option")
+      .property("value", id)
+      .text(id);
+  });
+
+  bar(data[0]);
+  gauge(data[0]);
+  bubble(data[0]);
+});
+
+
